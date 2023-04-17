b930e9777f03c8dfeea67cc74adfd5f3955c4117
Vakoi <mak-uk@mail.ru>
2023-04-17 16:09:40 +0300

    Добавьте файлы проекта.

---
+++ Практика день 1 (3.1)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int b = int.Parse(Console.ReadLine());
+
+        if (a < b)
+        {
+            Console.WriteLine("1");
+        }
+        else
+        {
+            Console.WriteLine("2");
+        }
+    }
+}
